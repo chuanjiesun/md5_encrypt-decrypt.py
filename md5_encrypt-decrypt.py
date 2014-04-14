b6f80b15f59c1364a95fee5671e0296cd68d61a2
chuanjiesun <chuanjiesun@gmail.com>
2014-04-14 13:50:51 +0800

    Update md5_encrypt-decrypt.py

--- md5_encrypt-decrypt.py
+++ md5_encrypt-decrypt.py
@@ -27,7 +27,6 @@ def md5_encrypt(clear_text_shuru) :
 
 def md5_decrypt(encrypt_string):
 	headers_somd5 = {
-	#POST /somd5-index-md5.html HTTP/1.1
 	'x-requested-with': 'XMLHttpRequest',
 	'Accept-Language': 'zh-cn',
 	'Referer': 'http://www.somd5.com/',
@@ -36,11 +35,8 @@ def md5_decrypt(encrypt_string):
 	'Accept-Encoding': 'gzip, deflate',
 	'User-Agent': 'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0)',
 	'Host': 'www.somd5.com',
-	#Content-Length: 75 #35+encrypt_string
 	'Proxy-Connection': 'Keep-Alive',
 	'Pragma': 'no-cache'
-	#Cookie: PHPSESSID=ics76oma7inepprp41lsklud36; AJSTAT_ok_times=1; AJSTAT_ok_pages=4; Hm_lpvt_ea66a7f6affa99ee3105d60145d6810d=1397391974; PHPSESSID=6ti1lf38dnlqi8ntsj1uk533i2; Hm_lvt_ea66a7f6affa99ee3105d60145d6810d=1397386712; AJSTAT_ok_pages=3; AJSTAT_ok_times=1
-	#isajax=QoG29V7X6mEGHt6Ep8pTI43&md5=40bd001563085fc35165329ea1ff5c5ecbdbbeef
 	}
 	url_somd5 = 'http://www.somd5.com/somd5-index-md5.html'
 	data = 'isajax=QoG29V7X6mEGHt6Ep8pTI43&md5='+encrypt_string
@@ -49,9 +45,7 @@ def md5_decrypt(encrypt_string):
 	req1 = opener.open(url_somd5)
 	resp1 = req1.read()
 	for a in cj:
-		#print(a.name)
 		cookie_name = a.name
-		#print(a.value)
 		cookie_value = a.value
 	length = 35+len(encrypt_string)
 	data_encode = data.encode()
@@ -60,21 +54,17 @@ def md5_decrypt(encrypt_string):
 	req2.add_header('Content-Length',length)	
 	req2.add_header('Cookie',cookie_name+':'+cookie_value)
 	resp2 = request.urlopen(req2)
-	#print(resp2.info())
 	resp2 = resp2.read(1000)
-	#print(type(resp2))
-	#print(resp2)
+
 	try:
 		resp2_decode = resp2.decode(encoding = 'gb18030')
 	except:
 		resp2_decode = resp2.decode(encoding = 'utf-8')
 	
-	#print('\n\n\naaaaaaaaaaaaaaa', resp2_decode)
 	bs = beautifulsoup(resp2_decode)
-	#print(resp2_decode)
+
 	print('开始解密...')
 	for b in bs.find_all('h1'):
-		#print(b)
 		text = b.get_text()
 		print('解密后：{}'.format(text))
 		sys.exit()
@@ -87,8 +77,7 @@ def canshu():
 			\nexample:%(prog)s -e clear_text\
 			\n	%(prog)s -d encrypt_crypt
 			''')
-	#parser.add_argument('-e', dest = 'clear text', help = 'specify a string to encrypt')
-	#parser.add_argument('-d', dest = 'encrypt string', help = 'specify a string to decrypt'
+
 	group = parser.add_mutually_exclusive_group()
 	group.add_argument('-e', dest = 'clear_text', help = 'specify a string to encrypt')
 	group.add_argument('-d', dest = 'encrypt_string', help = 'specify a string to decrypt')
@@ -101,7 +90,6 @@ def canshu():
 		else:
 			flag = False#加密
 			return args.encrypt_string, flag
-		#print(args.clear_text)
 	else:
 		parser.print_help()
 		sys.exit()
@@ -110,12 +98,10 @@ def canshu():
 def main(string_shuru, flag_panduan): 
 	if flag_panduan:
 		clear_text_shuru = string_shuru 
-	#string_encrypt = string_encrypt.encode()
 		md5_encrypt(clear_text_shuru)
 	else:
 		md5_decrypt(string_shuru)
 
 if __name__ == '__main__' :
 	canshu_return, flag_return =  canshu()
-	#print(canshu_return, flag_return)#202cb962ac59075b964b07152d234b70 False
 	main(canshu_return, flag_return)
